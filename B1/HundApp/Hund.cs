ae1215e5007540df7857d73423aa87bbe84fa08e
Lukas Müller <lukas.mueller@dozent.ipso.ch>
2023-09-12 10:49:59 +0200

    second commit

---
+++ B1/HundApp/Hund.cs
@@ -0,0 +1,66 @@
+﻿namespace HundApp
+{
+    /// <summary>
+    /// Aufgabe Modul 320
+    /// Klasse Hund implementieren
+    /// </summary>
+    public class Hund
+    {
+        /// <summary>
+        /// Hunde name
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Alter des Hundes in Jahren
+        /// </summary>
+        public int Age { get; set; } = 0;
+
+        /// <summary>
+        /// Hundrasse (Terrier, Boxer, Pudel, Dackel)
+        /// </summary>
+        public string Rasse { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Geschlecht des Hudes (M/W)
+        /// </summary>
+        public string Geschlecht { get; set; } = "M";
+
+        /// <summary>
+        /// Konstruktur
+        /// </summary>
+        public Hund() { }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="name">Name des Hundes</param>
+        /// <param name="age">Alter in Jahren</param>
+        /// <param name="rasse">Hunderasse</param>
+        /// <param name="geschlecht">Geschlecht M/W</param>
+        public Hund(string name, int age, string rasse, string geschlecht)
+        {
+            Name=name;
+            Age=age;
+            Rasse=rasse;
+            Geschlecht=geschlecht;
+        }
+
+        /// <summary>
+        /// Hund soll bellen
+        /// </summary>
+        public void Bellen()
+        {
+            Console.WriteLine($"{Name} bellt!");
+        }
+
+        /// <summary>
+        /// Hund soll fressen
+        /// </summary>
+        public void Fressen()
+        {
+            Console.WriteLine($"{Name} fristt!");
+        }
+
+    }
+}
---
+++ B1/HundApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HundApp
+{
+    /// <summary>
+    /// Übungsbeispiel Klassenmodul erstellen
+    /// Modul 320
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Hier beginnt das Programm (Einstiegspunkg)
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Bello
+            Hund bello = new Hund("Bello", 4, "Boxer", "M");
+            bello.Bellen();
+            bello.Fressen();
+
+            // Rocky
+            Hund rocky = new Hund("Rocky", 2, "Labrador Retriever", "M");
+            rocky.Bellen();
+            rocky.Fressen();
+
+            Console.WriteLine("Programm beenden mit <ENTER>");
+        }
+    }
+}
