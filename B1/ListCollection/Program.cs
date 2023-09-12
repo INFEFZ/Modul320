49dfde6df90785a9199e3fef0e4d12ee5e3a7aa6
Lukas Müller <lukas.mueller@dozent.ipso.ch>
2023-09-12 15:11:54 +0200

    List of names project added

---
+++ B1/ListCollection/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ListCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+
+            // Variante 1
+            names.Add("Peter");
+            names.Add("Alina");
+            names.Add("Sabine");
+            names.Add("Florian");
+
+            // Variante 2
+            // names.AddRange(new string[] { "Peter", "Alina", "Sabine", "Florian"});
+
+            // alle Namen ausgeben
+            Console.WriteLine("Unsortiert");
+            Console.WriteLine("------------------------");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            // erstes Element
+            string nFirst = names[0];
+
+            // letztes Element
+            string nLast = names[names.Count - 1];
+
+            names.Sort();
+            Console.WriteLine("\n");
+            Console.WriteLine("Sortiert");
+            Console.WriteLine("------------------------");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("Weiter mit <Enter>");
+        }
+    }
+}
