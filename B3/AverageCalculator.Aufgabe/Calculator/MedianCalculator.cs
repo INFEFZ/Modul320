521dcaa795dc9982037fd436cbe5714d6c7fc5b9
Lukas Müller <lukas.mueller@dozent.ipso.ch>
2023-09-19 15:05:51 +0200

    b3-testing

---
+++ B3/AverageCalculator.Aufgabe/Calculator/MedianCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculator
+{
+    public class MedianCalculator
+    {
+        public double CalculateMedian(double[] numbers)
+        {
+            var numbersCloned = (double[])numbers.Clone();
+            Array.Sort(numbersCloned);
+            var size = numbersCloned.Length;
+            var mid = size / 2;
+
+            if (size % 2 != 0)
+                return numbersCloned[mid];
+
+            var midValue1 = numbersCloned[mid];
+            var midValue2 = numbersCloned[mid - 1];
+            return (midValue1 + midValue2) / 2;
+        }
+    }
+}
