20dde0d2108fd776be39fdd20322dcabe55135f3
Lukas Müller <lukas.mueller@dozent.ipso.ch>
2023-09-19 16:01:10 +0200

    b3-testing-loesung

---
+++ B3/AverageCalculator.Loesung/Calculator.Tests/AverageCalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Calculator.Tests
+{
+    public class AverageCalculatorTests
+    {
+        [Fact]
+        public void Calculates_Average()
+        {
+            //Arrange
+            var sut = new AverageCalculator();
+            var input = new[] { 5.0, 10.0, 15.0 };
+
+            //Act
+            var result = sut.CalculateAverage(input);
+
+            //Assert
+            Assert.Equal(10, result);
+        }
+    }
+}
---
+++ B3/AverageCalculator.Loesung/Calculator/AverageCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator
+{
+    public class AverageCalculator
+    {
+        public double CalculateAverage(double[] numbers)
+        {
+            return numbers.Sum() / numbers.Length;
+        }
+    }
+}
